ad577e9e4ca404329f61c8a036608df5f27288b2
burneverything1 <bjyaboy@gmail.com>
2021-08-26 01:23:10 +0000

    momentum backtest class finish

--- mom-vector-backtester.py
+++ mom-vector-backtester.py
@@ -42,3 +42,42 @@ class MomVectorBacktester(object):
         raw.rename(columns={self.symbol: 'price'}, inplace = True)
         raw['return'] = np.log(raw / raw.shift(1))
         self.data = raw
+
+    def run_strategy(self, momentum = 1):
+        '''Backtests the trading strategy.'''
+        self.momentum = momentum
+        data = self.data.copy().dropna()
+        data['position'] = np.sign(data['return'].rolling(momentum).mean())
+        data['strategy'] = data['position'].shift(1) * data['return']
+
+        # determine when a trade takes place
+        data.dropna(inplace=True)
+        trades = data['position'].diff().fillna(0) != 0
+
+        # subtract transaction costs from return when trade takes place
+        data['strategy'][trades] -= self.tc
+        data['creturns'] = self.amount * data['return'].cumsum().apply(np.exp)
+        data['cstrategy'] = self.amount * data['strategy'].cumsum().apply(np.exp)
+        self.results = data
+
+        # absolute performance of the strategy
+        aperf = self.results['cstrategy'].iloc[-1]
+
+        # out-/underperformance of strategy
+        operf = aperf - self.results['creturns'].iloc[-1]
+
+        return round(aperf, 2), round(operf, 2)
+
+    def plot_results(self):
+        '''Plots the cumulative performance of the trading strategy compared to the symbol.'''
+        if self.results is None:
+            print('No results to plot yet. Run a strategy.')
+        title = '%s | TC = %.4f' % (self.symbol, self.tc)
+        self.results[['creturns', 'cstrategy']].plot(title=title, figsize=(10, 6))
+
+if __name__ = '__main__':
+    mombt = MomVectorBacktester('XAU=', '2010-1-1', '2020-12-31', 10000, 0.0)
+    print(mombt.run_strategy())
+    print(mombt.run_strategy(momentum=2))
+    mombt = MomVectorBacktester('XAU=', '2010-1-1', '2020-12-31', 10000, 0.001)
+    print(mombt.run_strategy(momentum=2))
